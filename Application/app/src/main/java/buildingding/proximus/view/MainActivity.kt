0c312863d7ba1fd4ee5367b4bf2fa34adf281e66
Johan Molenberghs <johan.molenberghs@student.ucll.be>
2020-09-29 11:17:35 +0200

    [BDD-47] populate spinner in MainActivity.kt from csv file in assets, not yet cast to location

--- Application/app/src/main/java/buildingding/proximus/view/MainActivity.kt
+++ Application/app/src/main/java/buildingding/proximus/view/MainActivity.kt
@@ -2,11 +2,27 @@ package buildingding.proximus.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 import buildingding.proximus.R
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val file_name = "locations.csv"
+        val inputStream = application.assets.open(file_name)
+        val rows = csvReader().readAll(inputStream)
+        val spinner_start = findViewById<Spinner>(R.id.spinner_start)
+        val spinner_end = findViewById<Spinner>(R.id.spinner_end)
+        if (spinner_start != null && spinner_end != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, rows
+            )
+            spinner_start.adapter = adapter
+            spinner_end.adapter = adapter
+        }
     }
+
 }
