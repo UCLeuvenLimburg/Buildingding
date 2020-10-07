d1eb44d4930a56bb5a2b67ff3eea2c7709f70e73
r0705835 <32357167+r0705835@users.noreply.github.com>
2020-10-07 16:44:45 +0200

    refactor: variable name change to LocationTest

--- Application/app/src/test/java/buildingding/proximus/model/LocationTest.kt
+++ Application/app/src/test/java/buildingding/proximus/model/LocationTest.kt
@@ -3,7 +3,6 @@ package buildingding.proximus.model
 import org.junit.Assert
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -11,7 +10,7 @@ internal class LocationTest {
     private val c001Name = "c001"
     private val c001 = Location(c001Name,Floor.C0)
     private val c002Name = "c001"
-    private val c001_otherFloor = Location(c001Name,Floor.C1)
+    private val c001OtherFloor = Location(c001Name,Floor.C1)
     private val c002 = Location(c002Name,Floor.C0)
 
 
@@ -27,7 +26,7 @@ internal class LocationTest {
 
     @Test
     fun testSameNameDifferentFloorIsNotEqual() {
-        Assert.assertNotEquals(c001,c001_otherFloor)
+        Assert.assertNotEquals(c001,c001OtherFloor)
     }
 
     @Test
