a89cafc9033fa11b0a0d7fa9dfeee240a0556218
Johan Molenberghs <johan.molenberghs@student.ucll.be>
2020-10-09 16:14:10 +0200

    MainActivity leads to menu

--- Application/app/src/main/java/buildingding/proximus/view/MainActivity.kt
+++ Application/app/src/main/java/buildingding/proximus/view/MainActivity.kt
@@ -1,8 +1,11 @@
 package buildingding.proximus.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
 import buildingding.proximus.R
 import buildingding.proximus.model.Dijkstra
 import buildingding.proximus.model.Graph
@@ -10,13 +13,21 @@ import buildingding.proximus.model.Location
 import buildingding.proximus.repository.LocationRepository
 
 class MainActivity : AppCompatActivity() {
+    lateinit var constraintLayout: ConstraintLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val locationRepository = LocationRepository()
+        constraintLayout = findViewById(R.id.mainLayout)
+        constraintLayout.setOnClickListener {
+            val intent = Intent(this@MainActivity, MenuActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        /*val locationRepository = LocationRepository()
         locationRepository.readLocationsFromCSV(application.assets.open("test_locations.csv"))
-        locationRepository.readConnectionsFromCSV(application.assets.open("neighbours.csv"))
-        val rows = locationRepository.locations.toList()
+        locationRepository.readConnectionsFromCSV(application.assets.open("neighbours.csv"))*/
+        /*val rows = locationRepository.locations.toList()
         val spinnerStart = findViewById<Spinner>(R.id.spinner_start)
         val spinnerEnd = findViewById<Spinner>(R.id.spinner_end)
         if (spinnerStart != null && spinnerEnd != null) {
@@ -29,13 +40,13 @@ class MainActivity : AppCompatActivity() {
         }
         val nodes = locationRepository.locations.toList().associateBy({ it.id }, { it })
         val graph = Graph(nodes as HashMap<Int, Location>)
-        val dijkstra = Dijkstra(graph.getPriorityQueue())
+        val dijkstra = Dijkstra(graph.getPriorityQueue())*/
         /*
         println("\n All paths: \n")
         println(locationRepository.getLocationByName("C004")?.id?.let { dijkstra.calculatePaths(it, graph.nodes) })
          */
-        val start = locationRepository.getLocationByName("C001")?.id
+        /*val start = locationRepository.getLocationByName("C001")?.id
         val end = locationRepository.getLocationByName("C004")?.id
-        val path = start?.let { end?.let { it1 -> dijkstra.getPath(it, it1, nodes) } }
+        val path = start?.let { end?.let { it1 -> dijkstra.getPath(it, it1, nodes) } }*/
     }
 }
