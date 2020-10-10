d8238a9dc3f32b7bac85af4881a4cada3fddca2c
Johan Molenberghs <johan.molenberghs@student.ucll.be>
2020-10-10 07:36:31 +0200

    added empty activities

---
+++ Application/app/src/main/java/buildingding/proximus/view/Navigation2DActivity.kt
@@ -0,0 +1,12 @@
+package buildingding.proximus.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import buildingding.proximus.R
+
+class Navigation2DActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation2_d)
+    }
+}
