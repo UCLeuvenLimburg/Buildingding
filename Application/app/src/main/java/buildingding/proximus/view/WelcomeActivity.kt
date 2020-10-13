ee21b853d500469e136092931da2606bbc91edff
Johan Molenberghs <johan.molenberghs@student.ucll.be>
2020-10-13 14:09:32 +0200

    WelcomeActivity.tk override onStop to remove callback

--- Application/app/src/main/java/buildingding/proximus/view/WelcomeActivity.kt
+++ Application/app/src/main/java/buildingding/proximus/view/WelcomeActivity.kt
@@ -10,6 +10,8 @@ import buildingding.proximus.model.Location
 import buildingding.proximus.repository.LocationRepository
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var mHandler: Handler
+    private lateinit var mRunnable: Runnable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +28,16 @@ class WelcomeActivity : AppCompatActivity() {
     }
 
     private fun startMenuActivity() {
-        val mRunnable = Runnable {
+        mRunnable = Runnable {
             startActivity(Intent(this, MenuActivity::class.java))
             finish()
         }
-        val mHandler = Handler()
+        mHandler = Handler()
         mHandler.postDelayed(mRunnable, 2000)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mHandler.removeCallbacks(mRunnable)
     }
 }
