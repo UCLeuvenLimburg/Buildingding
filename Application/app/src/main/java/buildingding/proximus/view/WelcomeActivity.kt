2e3faf1c41c656b772c5f1204e164c17144b64ac
Johan Molenberghs <johan.molenberghs@student.ucll.be>
2020-10-13 14:02:07 +0200

    activity_welcome is shown for 2 seconds

--- Application/app/src/main/java/buildingding/proximus/view/WelcomeActivity.kt
+++ Application/app/src/main/java/buildingding/proximus/view/WelcomeActivity.kt
@@ -3,24 +3,26 @@ package buildingding.proximus.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import androidx.constraintlayout.widget.ConstraintLayout
 import buildingding.proximus.R
 import buildingding.proximus.model.Location
 import buildingding.proximus.repository.LocationRepository
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        initiateContent()
+        setContentView(R.layout.activity_welcome)
+        //initiateContent()
         initiateLocations()
+        startMainActivity()
     }
 
     private fun initiateContent() {
         val constraintLayout:ConstraintLayout = findViewById(R.id.mainLayout)
         constraintLayout.setOnClickListener {
-            val intent = Intent(this@MainActivity, MenuActivity::class.java)
+            val intent = Intent(this@WelcomeActivity, MenuActivity::class.java)
             startActivity(intent)
         }
     }
@@ -30,5 +32,14 @@ class MainActivity : AppCompatActivity() {
                 .readLocationsFromCSV(application.assets.open("locations.csv"))
                 .readConnectionsFromCSV(application.assets.open("neighbours.csv"))
                 .locations.sortedBy { it.name }.toList()
+    }
+
+    private fun startMainActivity() {
+        val mRunnable = Runnable {
+            startActivity(Intent(this, MenuActivity::class.java))
+            finish()
+        }
+        val mHandler = Handler()
+        mHandler.postDelayed(mRunnable, 2000)
     }
 }
