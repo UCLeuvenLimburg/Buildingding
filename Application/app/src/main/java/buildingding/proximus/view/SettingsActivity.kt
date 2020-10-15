c317cae19e5ccd02470f55b92c1c9e645893af39
r0705835 <32357167+r0705835@users.noreply.github.com>
2020-10-15 12:05:49 +0200

    Removed locationView option from setttings

--- Application/app/src/main/java/buildingding/proximus/view/SettingsActivity.kt
+++ Application/app/src/main/java/buildingding/proximus/view/SettingsActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import buildingding.proximus.R
 
 class SettingsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -20,7 +21,6 @@ class SettingsActivity : AppCompatActivity() {
         val linearLayoutLocations: LinearLayout = findViewById(R.id.scrollViewLayout)
         linearLayoutLocations.addView(createLanguage())
         linearLayoutLocations.addView(createStartChoice())
-        linearLayoutLocations.addView(createRouteDisplay())
     }
 
     private fun createLanguage(): TextView {
@@ -48,21 +48,6 @@ class SettingsActivity : AppCompatActivity() {
         textView.setPadding(0, 24, 0, 24)
         textView.setOnClickListener {
             val intent = Intent(this@SettingsActivity, ChooseStartChoice::class.java)
-            startActivity(intent)
-        }
-        return textView
-    }
-
-    private fun createRouteDisplay(): TextView {
-        val textView = TextView(this)
-        textView.text = getString(R.string.settings_option_route_display)
-        textView.textSize = 24f
-        textView.gravity = Gravity.CENTER
-        textView.setBackgroundColor(getColor(R.color.colorWhite))
-        textView.setTextColor(getColor(R.color.colorUcllDarkBlue))
-        textView.setPadding(0, 24, 0, 24)
-        textView.setOnClickListener {
-            val intent = Intent(this@SettingsActivity, ChooseRouteDisplay::class.java)
             startActivity(intent)
         }
         return textView
